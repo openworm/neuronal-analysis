b413aea0b9574c0bcbd2894ed26702e097a7b3b6
Akiva Lipshitz <aclscientist@gmail.com>
2016-04-07 19:24:13 -0400

    refactored kato neuron names reader

---
+++ src/example.py
@@ -0,0 +1,7 @@
+import katodata as kd
+
+d = kd.KatoData()
+# print d.wormData
+
+dfof = d.wormData[0]['deltaFOverF_deriv']
+# print dfof
--- src/katodata.py
+++ src/katodata.py
@@ -1,31 +1,24 @@
 import numpy as np
 import os
 import scipy.io as scio
+import timeseries as ts
 
 currdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 data_location = os.path.join(currdir, 'data/wbdata/')
 print data_location
 MAT_EXTENSION = '.mat'
 
+# TODO: prevent "---" from getting into the list of neurons
 def extract_nids_list(wormdata):
     nids = wormdata['NeuronIds'].transpose()
     total = []
+    print wormdata['FlNm']
     for x in nids:
         for j in x:
-            for y in j:
-                d = []
-                if len(y)==0: total.append(None)
-                else:
-                    for n in y:
-                        listed = n.tolist()
-                        if len(listed)>0: d.append(listed[0] )
-                    total.append(d)
-    # I'm too lazy to figure out why
-    # the lists come out with neuronids
-    # nested, so I'm just going to
-    # flatten it and hope the interpreter
-    # will optimize for me
-    
+            neuron_array = j[0]
+            neurons = [ extract for n in neuron_array for extract in n if extract !='-'*3]
+            total.append(neurons)
+
     return total
 
 def readfile(fname):
@@ -33,7 +26,7 @@ def readfile(fname):
     _, ext = os.path.splitext(fname)
     if(ext!= MAT_EXTENSION):
         # We'll need better errorchecking and raising
-        # I don't know the idiomatic way of doing this in 
+        # I don't know the idiomatic way of doing this in
         # python
         print("Error: Must pass Matfile")
         raise
@@ -54,41 +47,39 @@ def load_matfile(matfile):
 
     data=matfile['wbData'][0][0]
 
-    keyed_data = { 
-        data.dtype.names[i]: data[i] 
+    keyed_data = {
+        data.dtype.names[i]: data[i].T
         for i in range(len(data.dtype.names))
     }
     keyed_data['NeuronIds']  = extract_nids_list(keyed_data)
     return keyed_data
 
+def mat_dict_to_timeseries(mat_dict):
+   """ Wraps all the timeseries in timeseries objects """
+   neurons = mat_dict['NeuronIds']
+   data = { key:ts.TimeSeries(series,nnames=neurons) \
+   for key, series in mat_dict.iteritems() \
+   if key != 'NeuronIds' and key != 'FlNm'}
+
+   data['NeuronIds'] = np.array(mat_dict['NeuronIds'])
+   data['FlNm'] = mat_dict['FlNm'][0]
+   return data
+
 def loadfiles(files):
-    
-    datasets = { 
-        fname: load_matfile(filestr) 
-        for fname, filestr in files.iteritems()
-    }
-    
-    return datasets
+   """ Puts scipy-io sourced matfiles into a cleaner, more structured form """
+   datasets = [mat_dict_to_timeseries(load_matfile(filestr))
+       for fname, filestr in files.iteritems()]
+
+   return datasets
 
 
 class KatoData:
-          
+
+
     def __init__(self):
-        
+
         files = {
             fname:readfile(os.path.join(data_location,fname))
             for fname in os.listdir(data_location)}
-        print [(f) for f,k in files.iteritems()] 
+        print [(f) for f,k in files.iteritems()]
         self.wormData = loadfiles(files)
-
-    def random_dataset(self):
-        wd = self.wormData
-        return wd[wd.keys()[0]]
-        
-    
-    
-        
-        
-
-        
-
