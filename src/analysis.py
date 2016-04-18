4976fc39741718e961de843e425761a05c97d66b
Benjamin Jack <benjaminjack@gmail.com>
2016-04-18 18:43:42 -0400

    Numerical differentiation and Improvements to RNN
    - Implements numerical differentiation (Chartrand, et al.)
    as cited in Kato, using off-the-shelf script by Chartrand.
    Numerical differentiation is available as a Analyzer method 'tvd'
    
    - Demonstrates the use of numerical differentiation in a Jupyter
    notebook (KatoPipeline.ipynb). The notebook compares the results
    of PCA on Kato's provided derivative data vs computed derivative data
    
    - Implements Analyzer methods to quickly generate 'Kato-style'
    PCA plots and timeseries plots
    
    - RNN: based on feedback from @jrieke, attempts to implement
    GMMActivation and GMM Loss functions on RNN. However, the code
    is not currently working correctly, and is returning only NaNs
    when attempting to fit the model.

--- src/analysis.py
+++ src/analysis.py
@@ -16,9 +16,61 @@ Such capabilities will be invaluable if we want to develop complex auto-experime
 software. 
 
 """
+
+import sys
+import os
+import numpy as np
+
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+
+from tvregdiff import TVRegDiff
+
 class Analyzer:
 
     def __init__(self, timeseries):
         self.timeseries = timeseries
 
-    
+    def tvd(self, iters=10, alpha=1e-1, diff=None): #iter specifies number of iterations, alpha specifies 'regularization parameter'
+    	
+    	deriv = []
+    	count = 0
+
+    	for t, vector in enumerate(self.timeseries.T): #differentiate neuron-by-neuron
+
+    		u = TVRegDiff(vector, iters, alpha, dx=diff, plotflag=0, diagflag=0)
+    		deriv.append(u)
+
+    		if count % 10 == 0:
+    			print("Completed:", count)
+    		count += 1
+
+    	return np.array(deriv).T
+
+    def timeseries_plot(self, ax):
+    	subplot = ax.pcolor(self.timeseries.T)
+    	return subplot
+
+    def pca_plot3d(self, ax):
+
+    	# Example:
+    	# a = Analyzer(wormData)
+    	# fig = plt.figure(figsize=(20,10))
+    	# ax = fig.add_subplot(1, 2, 1, projection='3d')
+    	# a.pca_plot3d(ax)
+    	# ax.set_title(("PCA results"))
+    	# plt.show()
+
+		pca = PCA(n_components=3)
+		pca.fit(self.timeseries)
+		out = pca.transform(self.timeseries)
+
+		col = (out.T[0] + out.T[1] + out.T[2])
+
+		cmhot = plt.get_cmap("jet")
+
+		subplot = ax.scatter(out.T[0], out.T[1], out.T[2], depthshade='False', c=col, cmap=cmhot, marker='o')
+
+		return subplot
+
