6232570158203cc6fb571bfadcfdabd6a8a62e68
Akiva Lipshitz <aclscientist@gmail.com>
2016-04-19 14:18:51 -0400

    added connectome to data_config and  started d some new lab-experiments, i.e. notebooks

--- src/data_config.py
+++ src/data_config.py
@@ -6,7 +6,7 @@ import pandas as pd
 manager = dm.BioDataManager()
 
 kato_matlab =  dm.BioDataset(
-    name="Kato Matlab  Dataset",
+    name="Kato Matlab",
     filepath=kd.data_location,
     annotation='Deprecated. Use Kato Dataset instead',
     tags=['katodata'],
@@ -15,10 +15,22 @@ kato_matlab =  dm.BioDataset(
 
 flnm = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))+'/data/katodata.pickle'
 kato = dm.BioDataset(
-    name="Kato Dataset",
+    name="Kato",
     filepath=flnm,
     readfunc=pd.read_pickle,
     writefunc=lambda flnm, data: data.to_pickle(flnm),
     annotation='Straight from the heads of five worms to your table',
+    manager=manager,
+    autoload=True)
+
+projdir=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+connectome_excel=dm.BioDataset(
+    name='Connectome Excel',
+    filepath=projdir+'/data/CElegansNeuronTables.xls',
+    readfunc=lambda flnm: pd.read_excel(flnm, sheetname=None),
+    writefunc=lambda flnm, data: data.write_excel(flnm),
+    annotation='The CElegans Connectome Excel',
     manager=manager,
     autoload=True)
